a2150b9ea8fc194235a57de60ea5ee2910afbcfa
master-dhanesh <dhanesh1296@gmail.com>
2023-01-28 19:25:36 +0530

    Two Way Binding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const App = () => {
-    let [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
 
-    const clickHandler = () => {
-        setTime(new Date().toLocaleTimeString());
+    const submithandler = (e) => {
+        e.preventDefault();
+        // console.log(e.target[0].value);
+        // console.log(e.target[1].value);
+        console.log({ title, desc });
+        setTitle("");
+        setDesc("");
     };
-
-    setInterval(() => {
-        clickHandler();
-    }, 1000);
 
     return (
         <div>
-            <h1>Live Time: {time}</h1>
-            <button onClick={clickHandler}>Change Name</button>
+            <form onSubmit={submithandler}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <input
+                    type="text"
+                    placeholder="Description"
+                    onChange={(e) => setDesc(e.target.value)}
+                    value={desc}
+                />
+                <button>Create Task</button>
+            </form>
         </div>
     );
 };
