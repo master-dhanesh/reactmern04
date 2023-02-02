7459765806ada5e60c2d8f993a9c28b6e4ff75e8
master-dhanesh <dhanesh1296@gmail.com>
2023-02-02 19:09:12 +0530

    Lifecycle  Class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { useState } from "react";
-import Create from "./components/Create";
+import React, { Component } from "react";
 import Show from "./components/Show";
 
-const App = () => {
-    const [tasks, setTasks] = useState([]);
-    return (
-        <div>
-            <Create tasks={tasks} setTasks={setTasks}>
-                This is Tags Information.
-            </Create>
-            <hr />
-            <Show tasks={tasks} setTasks={setTasks} />
-        </div>
-    );
-};
+class App extends Component {
+    // state = {};
+    // creation - mounting
+    constructor(props) {
+        // to init compoennt state/data
+        super(props);
+        console.log("[App.js] constructor");
+        this.state = {
+            images: [],
+            show: false,
+        };
+    }
+
+    render() {
+        // to load view(html/css without original data)
+        // render will reload everytime when the view changes due to data
+        console.log("[App.js] render");
+
+        return (
+            <div>
+                <h1>This is App.js</h1>
+                <hr />
+                {this.state.show ? <Show /> : "No Show"}
+                <button
+                    onClick={() => this.setState({ show: !this.state.show })}
+                >
+                    Click
+                </button>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        // data calling through api/internet
+        // runs one in a lifetime in a component
+        // as the data arrives, set the data into the state
+        fetch("https://picsum.photos/v2/list")
+            .then((d) => d.json())
+            .then((data) => {
+                // console.log(data);
+                this.setState({ images: data });
+            });
+        console.log("[App.js] componentdidmount");
+    }
+    // -------------------------------------------------------
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevProps, prevState);
+        console.log("[App.js] componentdidupdate");
+    }
+    // ------------------------------------------------------
+}
 
 export default App;
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,32 +1,11 @@
-import React from "react";
-import { useAlert } from "react-alert";
-import "./Show.css";
+import React, { Component } from "react";
 
-const Show = (props) => {
-    const alert = useAlert();
-
-    const { tasks, setTasks } = props;
-
-    const taskdelete = (i) => {
-        const copytasks = [...tasks];
-        copytasks.splice(i, 1);
-        setTasks(copytasks);
-        alert.show("Task removed!");
-    };
-
-    let tasklist = <h1>No Tasks Found!</h1>;
-    if (tasks.length > 0) {
-        tasklist = tasks.map((task, index) => {
-            return (
-                <li key={index}>
-                    {task.title} -{" "}
-                    <span onClick={() => taskdelete(index)}>❌</span>
-                </li>
-            );
-        });
+export default class Show extends Component {
+    componentWillUnmount() {
+        console.log("[Show.js] componentwillunmount");
+        window.confirm("do you want to leave this page");
     }
-
-    return <ol>{tasklist}</ol>;
-};
-
-export default Show;
+    render() {
+        return <div>Show</div>;
+    }
+}
