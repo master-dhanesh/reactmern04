64ece285ca59e754439bdd4c029be4a7969a5eb3
master-dhanesh <dhanesh1296@gmail.com>
2023-02-04 19:27:37 +0530

    Gallery 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import loader from "./assets/loader.gif";
+import ReactPaginate from "react-paginate";
+import "./App.css";
 
 const App = () => {
     const [images, setImages] = useState(null);
 
-    const getImages = () => {
-        fetch("https://picsum.photos/v2/list")
-            .then((d) => d.json())
-            .then((data) => setImages(data))
-            .catch((err) => console.log(err));
+    const getimages = async (count = 1) => {
+        console.log(count);
+        try {
+            const { data } = await axios.get(
+                `https://picsum.photos/v2/list?page=${count}&limit=9`
+            );
+            setImages(data);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     useEffect(() => {
-        getImages();
-    }, [images]);
+        getimages();
+    }, []);
+
+    const onpagechange = ({ selected }) => {
+        getimages(selected + 1);
+    };
 
     return (
-        <div>
-            {/*  */}
-            <button onClick={getImages}>Get Images</button>
-            {images ? JSON.stringify(images) : "loading..."}
-            {/*  */}
+        <div
+            style={{ height: "100vh" }}
+            className="container d-flex w-100 justify-content-center align-items-center flex-column "
+        >
+            {images ? (
+                <div>{JSON.stringify(images)}</div>
+            ) : (
+                <img style={{ width: "30vmax" }} src={loader} alt="" />
+            )}
+
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel=">>"
+                onPageChange={onpagechange}
+                pageRangeDisplayed={5}
+                pageCount={10}
+                previousLabel="<<"
+            />
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
